b4e590c384fc333dae098c3f9318c115f921a824
Ramitha14 <51942958+Ramitha14@users.noreply.github.com>
2020-10-29 07:20:58 +0530

    Add files via upload

---
+++ Java/Activity_1_1.java
@@ -0,0 +1,12 @@
+
+public class Activity_1_1 {
+	public static void main(String[] args) {
+		Car skoda = new Car();
+        skoda.make = 2014;
+        skoda.color = "Black";
+        skoda.transmission = "Manual";
+        skoda.displayCharacterstics();
+        skoda.accelerate();
+        skoda.brake();
+	}
+}
